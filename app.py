77759f0b5af5f08e6d9ab976f922d0b617f7c585
sugurusato5 <jt.ag.dm.2782@gmail.com>
2026-08-16 20:09:29 +0900

    app.py変更

--- app.py
+++ app.py
@@ -1,24 +1,21 @@
 import os
-from flask import Flask
 from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
 from dotenv import load_dotenv
-  
+
 load_dotenv(override=True)
-  
+
 app = Flask(__name__)
-  
+
 line_bot_api = LineBotApi(os.environ["ACCESS_TOKEN"])
 handler = WebhookHandler(os.environ["CHANNEL_SECRET"])
 
-  
-  @app.route("/")
-  def index():
-      return "You call index()"
-  
-  
+@app.route("/")
+def index():
+    return "You call index()"
+
 @app.route("/callback", methods=["POST"])
 def callback():
     """Messaging APIからの呼び出し関数"""
@@ -44,6 +41,6 @@ def handle_message(event):
     line_bot_api.reply_message(event.reply_token, TextSendMessage(text=event.message.text))
 
 
-  if __name__ == "__main__":
-      port = int(os.getenv("PORT", 5000))
-      app.run(host="0.0.0.0", port=port)
+if __name__ == "__main__":
+    port = int(os.getenv("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
